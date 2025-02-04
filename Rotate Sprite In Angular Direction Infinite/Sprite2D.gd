99b4c07fe420029171a41dfccf67e9e533f24572
Nitesh Kumar <nitesh098suthar@gmail.com>
2025-02-04 14:56:34 +0530

    add button to play and pause the animation using signals

--- Rotate Sprite In Angular Direction Infinite/Sprite2D.gd
+++ Rotate Sprite In Angular Direction Infinite/Sprite2D.gd
@@ -7,6 +7,10 @@ var angular_speed = PI
 var speed = 400
 
 func _process(delta):
-	rotation -= angular_speed * delta #use + or - to change the direction
+	rotation += angular_speed * delta #use + or - to change the direction
 	var velocity = Vector2.UP.rotated(rotation)
 	position += speed * velocity * delta # Update position correctly
+
+
+func _on_button_pressed():
+	set_process(not is_processing()) # Replace with function body.
